3313b70070dbf6ce0d56e7f75712ec505e5a29c7
dizvyagintsev <dizvyagintsev@gmail.com>
2024-09-08 18:19:24 +0200

    add pagination

--- voting/views/restaurant.py
+++ voting/views/restaurant.py
@@ -1,9 +1,16 @@
-from drf_spectacular.utils import OpenApiResponse, extend_schema
+from drf_spectacular.utils import OpenApiResponse, extend_schema, OpenApiParameter
 from rest_framework import generics
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import AllowAny
 
 from voting.repositories.restaurant import RestaurantRepository
 from voting.serializers import RestaurantSerializer
+
+
+class RestaurantPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 50
 
 
 @extend_schema(
@@ -15,9 +22,14 @@ from voting.serializers import RestaurantSerializer
             description="Invalid input data or missing required fields."
         ),
     },
+    parameters=[
+        OpenApiParameter(name='page', description='Page number for pagination', required=False, type=int),
+        OpenApiParameter(name='page_size', description='Number of items per page', required=False, type=int),
+    ],
 )
 class RestaurantListCreateView(generics.ListCreateAPIView):
     permission_classes = [AllowAny]
+    pagination_class = RestaurantPagination
 
     queryset = RestaurantRepository.all()
     serializer_class = RestaurantSerializer
